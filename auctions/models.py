0bbd09774e782bd22b282fad3c159374b357b56f
martin-ivorycc <martin.klotzman@ivorycc.com>
2023-06-09 10:59:35 -0700

    create models and update admin.py

--- auctions/models.py
+++ auctions/models.py
@@ -4,3 +4,39 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Category(models.Model):
+    name = models.CharField(max_length=64, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Listing(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.TextField()
+    start_bid = models.DecimalField(max_digits=7, decimal_places=2)
+    image_url = models.URLField(blank=True, null=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True, related_name="listings")
+    listing_status = models.BooleanField(default=True)  # True for active, False for closed
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="listings")
+    winner = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True, related_name="wins")
+
+    def __str__(self):
+        return self.title
+
+class Bid(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bids")
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="bids")
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.user.username}'s bid on {self.listing.title}"
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="comments")
+    text = models.TextField()
+    time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s comment on {self.listing.title}"
